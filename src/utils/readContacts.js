7ca2ddc29edea286ba4c594d971be86c6ddb46d9
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-02-21 17:02:16 +0200

    add readContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,18 @@
-import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-export const readContacts = async () => {};
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const PATH_DB = path.resolve(__dirname, '../db/db.json');
+
+async function readContacts() {
+    try {
+        const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+        const contacts = JSON.parse(data);
+        console.log(contacts);
+        return contacts;
+    } catch (error) {
+        console.error('Error reading the contacts file:', error);
+    }
+}
+
+readContacts();
